d2694614419719d373dec4830d1b5ce296eddd74
POBSIZ <gjehdtjr911@gmail.com>
2020-08-27 07:25:01 +0900

    fix some error

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const { getInfo, validateID } = require('ytdl-core');
 const { url } = require('inspector');
 const { deepStrictEqual } = require('assert');
 const { callbackify } = require('util');
+const { get } = require('request');
 
 app.use(cors());
 
@@ -31,52 +32,42 @@ app.get ( '/style.css', (req, res, next) => {
 // Youtube download function
 app.get('/downloadmp3', async (req, res, next) => {
 	try {
-		function AUDIOfunction(a_url){
-
-			function getTitleAudio (videoUrl){
-				return new Promise ((resolve, reject) => {
-				   ytdl.getBasicInfo (videoUrl, {format: 'mp4'},(err, info) => {
-					if (err) throw err;
-						a_title = info.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
-					resolve (info.title)
-					console.log(info.title)
-				   })
+		
+		function getTitleAudio (videoUrl){
+			return new Promise ((resolve, reject) => {
+				ytdl.getBasicInfo (videoUrl, {format: 'mp4'},(err, info) => {
+				if (err) throw err;
+					a_title = info.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+				resolve (info.title)
+				console.log(info.title)
 				})
-			 } 
-			
-			function downloadAUDIO(){			
-				res.header('Content-Disposition', `attachment; filename="${a_title}.mp3"`);
-				ytdl(a_url, {
-					format: 'mp3',
-					filter: 'audioonly',
-					quality: 'highestaudio',
-				}).pipe(res);
-			}
-
-			var a_title = '';
-			
-			getTitleAudio(a_url).then(downloadAUDIO).then(console.log(a_title+' LOAD'))
+			})
+			} 
+		
+		function downloadAUDIO(){			
+			res.header('Content-Disposition', `attachment; filename="${a_title}.mp3"`);
+			ytdl(list_url, {
+				format: 'mp3',
+				filter: 'audioonly',
+				quality: 'highestaudio',
+			}).pipe(res);
 		}
 
+		var a_title = '';
 		var list_url = req.query.url
 		var isit_playlist = list_url.indexOf('playlist')
+		var link_list = new Array();
 		
 		if (isit_playlist != -1){
-			var s_url = list_url;
-			var link_list = new Array();
-
-			ytlist(s_url, 'url').then(res => {
+			ytlist(list_url, 'url').then(res => {
 				link_list = res.data.playlist;
+				for(i = 0; i<link_list.length; i++){
+					getTitleAudio(link_list[i]).then(console.log(link_list[i]))
+				}
 			});
-			console.log(link_list.length)
-			
-			for(i = 0; i<link_list.length; i++){
-				console.log(link_list[i]);
-				// AUDIOfunction(link_list[i]).then() 
-			}
 		}
 		else{
-			AUDIOfunction(list_url);
+			getTitleAudio(list_url).then(downloadAUDIO)
 		}
 
 	} catch (err) {
@@ -86,34 +77,49 @@ app.get('/downloadmp3', async (req, res, next) => {
 
 app.get('/downloadmp4', async (req, res, next) => {
 	try {
+		
 		function getTitleVideo (videoUrl){
 			return new Promise ((resolve, reject) => {
-			   ytdl.getBasicInfo (videoUrl, {format: 'mp4'},(err, info) => {
+				ytdl.getBasicInfo (videoUrl, {format: 'mp4'},(err, info) => {
 				if (err) throw err;
-					v_title = info.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+					a_title = info.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
 				resolve (info.title)
 				console.log(info.title)
-			   })
+				})
 			})
-		 } 
+			} 
 		
 		function downloadVIDEO(){			
-			res.header('Content-Disposition', `attachment; filename="${v_title}.mp4"`);
-			ytdl(v_url, {
+			res.header('Content-Disposition', `attachment; filename="${a_title}.mp4"`);
+			ytdl(list_url, {
 				format: 'mp4',
 				quality: 'highestvideo',
 			}).pipe(res);
 		}
+
+		var a_title = '';
+		var list_url = req.query.url
+		var isit_playlist = list_url.indexOf('playlist')
+		var link_list = new Array();
 		
-		var v_url = req.query.url;
-		var v_title = '';
-		
-		getTitleVideo(v_url).then(downloadVIDEO)
+		if (isit_playlist != -1){
+			ytlist(list_url, 'url').then(res => {
+				link_list = res.data.playlist;
+				for(i = 0; i<link_list.length; i++){
+					getTitleVideo(link_list[i]).then(console.log(link_list[i]))
+				}
+			});
+		}
+		else{
+			getTitleVideo(list_url).then(downloadVIDEO)
+		}
 
 	} catch (err) {
 		console.error(err);
 	}
 });
+
+
 
 app.listen(PORT, () => {
 	console.log(`Server Works !!! At port ${PORT}`);
