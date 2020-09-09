fef62d0264197d2a3b96262e2732cf2591be585c
POBSIZ <gjehdtjr911@gmail.com>
2020-09-10 01:36:24 +0900

    THX MOORAD

--- server.js
+++ server.js
@@ -91,6 +91,53 @@ app.get('/downloadmp3', async (req, res, next) => {
 	}
 });
 
+app.get('/downloadmp4', async (req, res, next) => {
+	try {
+
+		function getTitleViedo (videoUrl){
+			return new Promise ((resolve, reject) => {
+				ytdl.getBasicInfo (videoUrl, {format: 'mp4'},(err, info) => {
+				if (err) throw err;
+					a_title = info.videoDetails.title.replace(/[^\x00-\x7F]/g, "");
+					console.log(info.title)
+				resolve (info.title)
+				})
+			})
+			} 
+		
+		function downloadVIDEO(){			
+			res.header('Content-Disposition', `attachment; filename="${a_title}.mp4"`);
+			ytdl(list_url, {
+				format: 'mp4',
+				quality: 'highestvideo',
+			}).pipe(res);
+		}
+
+		var a_title = 'a_title';
+		var list_url = req.query.url
+		var isit_playlist = list_url.indexOf('playlist')
+		// var link_list = new Array();
+
+		if (isit_playlist != -1){
+			var setLT = ytlist(list_url, 'url').then(res => {
+				link_list = res.data.playlist;
+				console.log(link_list)
+				for(i = 0; i<link_list.length; i++){
+					getTitleAudio(link_list[i])
+				}
+			}).then(res => {
+				ejs.renderFile(__dirname + '/views/temp.ejs', {link_list: link_list}, {}, function(err, str){if (err) console.log(err)});
+			})
+		}
+		else{
+			getTitleVideo(list_url).then(downloadVIDEO).then(console.log(a_title))	
+		}
+		
+	} catch (err) {
+		console.error(err);
+	}
+});
+
 app.listen(PORT, () => {
 	console.log(`Server Works !!! At port ${PORT}`);
 });
