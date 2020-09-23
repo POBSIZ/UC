fa115c94951837c59cba69340a7fb76c0e8c1133
POBSIZ <gjehdtjr911@gmail.com>
2020-09-23 11:33:44 +0900

    4556543232

--- server.js
+++ server.js
@@ -123,7 +123,8 @@ app.get('/downloadmp4', async (req, res, next) => {
 			res.header('Content-Disposition', `attachment; filename="${a_title}.mp4"`);
 			ytdl(list_url, {
 				format: 'mp4',
-				quality: 'highest',
+				filter: 'audioandvideo',
+				quality: 'highestvideo',
 			}).pipe(res);
 		}
 
